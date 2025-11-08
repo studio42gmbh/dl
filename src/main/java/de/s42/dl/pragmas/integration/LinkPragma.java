a823cd768de97d4d183dcc110709f3c70d13b4b6
Benjamin Schiller <bs@s42m.de>
2025-11-09 00:54:11 +0100

    Cleaned formatting and includes for LinkPragma

--- src/main/java/de/s42/dl/pragmas/integration/LinkPragma.java
+++ src/main/java/de/s42/dl/pragmas/integration/LinkPragma.java
@@ -1,19 +1,19 @@
 // <editor-fold desc="The MIT License" defaultstate="collapsed">
 /*
  * The MIT License
- * 
+ *
  * Copyright 2022 Studio 42 GmbH ( https://www.s42m.de ).
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,11 +25,9 @@
 //</editor-fold>
 package de.s42.dl.pragmas.integration;
 
-import de.s42.dl.pragmas.*;
 import de.s42.dl.DLCore;
 import de.s42.dl.exceptions.InvalidPragma;
-import de.s42.log.LogManager;
-import de.s42.log.Logger;
+import de.s42.dl.pragmas.AbstractDLPragma;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -42,8 +40,7 @@ import java.nio.file.Path;
 public class LinkPragma extends AbstractDLPragma
 {
 
-	private final static Logger log = LogManager.getLogger(LinkPragma.class.getName());
-
+	//private final static Logger log = LogManager.getLogger(LinkPragma.class.getName());
 	public final static String DEFAULT_IDENTIFIER = "link";
 
 	public LinkPragma()
